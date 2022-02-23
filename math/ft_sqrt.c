531764249297939685fd548624fbd8f24273c209
Jonas ALLERHAND <jallerha@e2r4p3.clusters.42paris.fr>
2022-02-23 15:22:48 +0100

    maths lib

---
+++ math/ft_sqrt.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 12:16:33 by jallerha          #+#    #+#             */
+/*   Updated: 2022/02/23 12:17:21 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_sqrt(int n)
+{
+	int			i;
+	int			result;
+
+	i = 0;
+	result = 0;
+	while (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+long	ft_sqrt_l(long n)
+{
+	int			i;
+	long		result;
+
+	i = 0;
+	result = 0;
+	while (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+long long	ft_sqrt_ll(long long n)
+{
+	int			i;
+	long long	result;
+
+	i = 0;
+	result = 0;
+	while (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		i++;
+	}
+	return (0);
+}
