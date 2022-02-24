b259906b56e8b3ac4c3139a360c3b1a44900f8d6
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-24 17:12:05 +0100

    removed useless var

--- math/ft_sqrt.c
+++ math/ft_sqrt.c
@@ -6,17 +6,15 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 12:16:33 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/23 12:17:21 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/02/24 14:49:01 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int n)
 {
-	int			i;
-	int			result;
+	int	i;
 
 	i = 0;
-	result = 0;
 	while (i * i <= n)
 	{
 		if (i * i == n)
@@ -29,10 +27,8 @@ int	ft_sqrt(int n)
 long	ft_sqrt_l(long n)
 {
 	int			i;
-	long		result;
 
 	i = 0;
-	result = 0;
 	while (i * i <= n)
 	{
 		if (i * i == n)
@@ -45,10 +41,8 @@ long	ft_sqrt_l(long n)
 long long	ft_sqrt_ll(long long n)
 {
 	int			i;
-	long long	result;
 
 	i = 0;
-	result = 0;
 	while (i * i <= n)
 	{
 		if (i * i == n)
