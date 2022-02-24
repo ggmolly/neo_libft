3f5a8d9c9212f70489eaf12c5e6b71f10a8ee2da
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-24 17:14:23 +0100

    optimized syscall

--- put/ft_putstr.c
+++ put/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:32:22 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/22 18:57:00 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/02/24 12:36:11 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 int	ft_putstr_fd(t_string string, int fd)
 {
-	while (*string.str)
-	{
-		write(fd, string.str, 1);
-		string.str++;
-	}
+	write(fd, string.str, string.len);
 	return (string.len);
 }
