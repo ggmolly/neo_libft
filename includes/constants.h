11b25b80280fc90489f1b1a85206cc896aa3b60b
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-26 19:14:50 +0100

    included boolean in constants

--- includes/constants.h
+++ includes/constants.h
@@ -6,12 +6,14 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:26:49 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/23 13:36:47 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/02/26 19:14:33 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
+
+# include "bool.h"
 
 # define STDIN 0
 # define STDOUT 1
