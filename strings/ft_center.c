21caf3c2384dab16673684aedee1f8ad902eb0a3
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-28 00:08:28 +0100

    ft_center implemented

---
+++ strings/ft_center.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_center.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 19:51:24 by jallerha          #+#    #+#             */
+/*   Updated: 2022/02/27 20:00:21 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/memory.h"
+#include "../includes/string.h"
+
+char	*ft_center(char *s, int padding)
+{
+	char	*output;
+	int		i;
+	int		j;
+	
+	i = 0;
+	j = 0;
+	if (!ft_malloc(&output, sizeof(char), ft_strlen(s) + padding))
+		return (NULL);
+	while (i < padding / 2)
+	{
+		output[i] = ' ';
+		i++;
+	}
+	while (s[j])
+	{
+		output[i] = s[j];
+		i++;
+		j++;
+	}
+	while (i < padding + ft_strlen(s))
+	{
+		output[i] = ' ';
+		i++;
+	}
+	output[i] = '\0';
+	return (output);
+}
