736d9a2691bb068c3a2c6902c6d733212fd8df54
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-28 22:29:41 +0100

    fixed dumb typo

--- strings/ft_split_lines.c
+++ strings/ft_split_lines.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 21:58:25 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/28 22:10:35 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/02/28 22:28:58 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ t_chain_lst	*ft_split_lines(char *str)
 	t_chain_lst	*output;
 
 	if (ft_strstr("\r\n", str) != NULL)
-		ft_split(output, "\r\n");
+		output = ft_split(str, "\r\n");
 	else
-		ft_split(output, "\n");
+		output = ft_split(str, "\n");
 	return (output);
 }
