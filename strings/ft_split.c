81b2a141a3653d46dcca59a925a8400cd22c71eb
Jonas ALLERHAND <jallerha@student.42.fr>
2022-02-28 22:31:25 +0100

    updated function call

--- strings/ft_split.c
+++ strings/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 19:32:03 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/28 22:01:46 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/02/28 22:31:07 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ t_chain_lst	*ft_split(char *str, char *word)
 	int			end;
 	int			sep_len;
 
-	output = ft_lst_init();
+	output = ft_chain_init();
 	sep_len = ft_strlen(word);
 	start = 0;
 	end = ft_index(str, word);
