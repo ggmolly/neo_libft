e5531e1ae43f18af5d3cdc5c2386d76d15f0eb7a
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-01 13:41:47 +0100

    deprecated old tests

--- tests/src/strset_tests.c
+++ tests/src/strset_tests.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:27:26 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/22 19:45:02 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/01 11:57:36 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,73 +28,5 @@ int main(void)
 		"777777777",
 		"888888888",
 	};
-	
-	for (int i = 0; i < 5; i++)
-	{
-		t_string a = ft_init_string(str[i]);
-		ft_strset(&a, '*');
-		switch (i)
-		{
-		case 0:
-			if (strcmp(a.str, "***") != 0)
-			{
-				printf("\033[31m%s\n", a.str);
-				return (1);
-			} else {
-				printf("\033[32m%s\n", a.str);
-			}
-			break;
-		case 1:
-			if (strcmp(a.str, "****") != 0)
-			{
-				printf("\033[31m%s\n", a.str);
-				return (1);
-			} else {
-				printf("\033[32m%s\n", a.str);
-			}
-			break;
-		case 2:
-			if (strcmp(a.str, "******") != 0)
-			{
-				printf("\033[31m%s\n", a.str);
-				return (1);
-			} else {
-				printf("\033[32m%s\n", a.str);
-			}
-			break;
-		case 3:
-			if (strcmp(a.str, "") != 0)
-			{
-				printf("\033[31m%s\n", a.str);
-				return (1);
-			} else {
-				printf("\033[32m%s\n", a.str);
-			}
-			break;
-		case 4:
-			if (strcmp(a.str, "*********") != 0)
-			{
-				printf("\033[31m%s\n", a.str);
-				return (1);
-			} else {
-				printf("\033[32m%s\n", a.str);
-			}
-			break;
-		}
-	}
-	puts("\033[32mOK");
-	for (int i = 5; i < 10; i++)
-	{
-		t_string a = ft_init_string(str[i]);
-		ft_strnset(&a, '*', 1);
-		// Check if first char is a *
-		if (a.str[0] != '*')
-		{
-			printf("\033[31m%s\n", a.str);
-			return (1);
-		} else {
-			printf("\033[32m%s\n", a.str);
-		}
-	}
 	return (0);
 }
