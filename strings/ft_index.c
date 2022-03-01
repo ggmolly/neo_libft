95060a22a48fa00072e88c7f897be4ea37e51fba
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-01 15:36:31 +0100

    Consistent name

--- strings/ft_index.c
+++ strings/ft_index.c
@@ -6,24 +6,24 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 19:34:27 by jallerha          #+#    #+#             */
-/*   Updated: 2022/03/01 14:22:05 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/01 15:27:44 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_index(char *haybale, char *needle)
+int	ft_index(char *haystack, char *needle)
 {
 	int	i;
 	int	j;
 	int	k;
 
 	i = 0;
-	while (haybale[i])
+	while (haystack[i])
 	{
 		j = 0;
 		k = i;
 		while (needle[j])
 		{
-			if (haybale[k] != needle[j])
+			if (haystack[k] != needle[j])
 				break ;
 			j++;
 			k++;
