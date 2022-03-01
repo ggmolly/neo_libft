656095798bcbc80e8dab364525a22372f5c9846b
Jonas ALLERHAND <jallerha@student.42.fr>
2022-03-01 13:42:07 +0100

    renewed ft_zfill

--- strings/ft_zfill.c
+++ strings/ft_zfill.c
@@ -6,20 +6,24 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 17:20:02 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/26 17:32:51 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/03/01 13:26:16 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/memory.h"
 #include "../includes/string.h"
 
-void	ft_zfill(char *s, int n)
+char	*ft_zfill(char *s, int n)
 {
-	int zeroes;
+	int		zeroes;
+	char	*output;
 
 	zeroes = n - ft_strlen(s);
 	if (zeroes <= 0)
-		return ;
-	ft_memmove(s + zeroes, s, ft_strlen(s) + 1);
-	ft_memset(s, '0', zeroes);
+		return (NULL);
+	if (!ft_malloc(&output, sizeof(char), ft_strlen(s) + zeroes + 1))
+		return (NULL);
+	ft_memset(output, '0', zeroes);
+	ft_strncpy(output + zeroes, s, ft_strlen(s));
+	return (output);
 }
