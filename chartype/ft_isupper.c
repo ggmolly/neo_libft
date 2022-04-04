5ffe3922c34095652e17082c01feabe8dc3edb66
Jonas ALLERHAND <jallerha@student.42.fr>
2022-04-04 15:03:09 +0200

    chartype's optimized

--- chartype/ft_isupper.c
+++ chartype/ft_isupper.c
@@ -6,11 +6,11 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 14:39:49 by jallerha          #+#    #+#             */
-/*   Updated: 2022/02/24 14:58:00 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/04/04 14:22:59 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	return (c >= 65 && c <= 90);
 }
