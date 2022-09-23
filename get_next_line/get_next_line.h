5cd5a4636f6ea6db0f3d285740d68e328914b1c4
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-23 13:44:00 +0200

    include protection and function prototype

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,9 +6,12 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:32:53 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/23 13:35:12 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/23 13:43:39 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
@@ -26,8 +29,12 @@
 #  define ERROR -1
 # endif
 
+char	*get_next_line(int fd);
+
 typedef struct s_file_buffer
 {
 	char	buf[BUFFER_SIZE + 1];
 	int		current_index;
 }	t_file_buffer;
+
+#endif
